af4bf32d4b170bdf409b2df86c8448373afd8aa6
Skarf <54850873+Skarf4U@users.noreply.github.com>
2023-10-09 14:01:12 +0300

    port 5000

--- server/server.js
+++ server/server.js
@@ -19,6 +19,6 @@ app.get("/api", async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log("Server started on port 3000");
+app.listen(5000, () => {
+  console.log("Server started on port 5000");
 });
